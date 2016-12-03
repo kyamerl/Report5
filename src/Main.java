1d49fb8df24466e21c9c3ff6dadc581b57622fd9
Yuichi Kamezawa <e165721@ie.u-ryukyu.ac.jp>
2016-12-03 22:43:43 +0900

    Git - add & commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Created by e165721 on 2016/12/03.
+ */
+public class Main {
+    public static void main(String[] args){
+        String str = null;
+        System.out.println(str.length());
+    }
+}
