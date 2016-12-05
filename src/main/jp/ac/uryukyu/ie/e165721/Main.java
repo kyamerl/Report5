67f2941a6d671037bebbd7f057a57eabe057de55
Yuichi Kamezawa <e165721@ie.u-ryukyu.ac.jp>
2016-12-05 13:43:50 +0900

    Git - add & commit

--- src/main/jp/ac/uryukyu/ie/e165721/Main.java
+++ src/main/jp/ac/uryukyu/ie/e165721/Main.java
@@ -1,6 +1,10 @@
+package jp.ac.uryukyu.ie.e165721;
+
 /**
  * Created by e165721 on 2016/12/03.
+ * ステップ1: コードの準備
  */
+
 public class Main {
     public static void main(String[] args){
         String str = null;
